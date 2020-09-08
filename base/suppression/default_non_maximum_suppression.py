ff9234653a5efbab64aea9f62761d5d49d248e39
Michael Hernandez <mhernan88@gmail.com>
2020-09-08 14:13:50 -0400

    Combining more loops for optimizations

--- base/suppression/default_non_maximum_suppression.py
+++ base/suppression/default_non_maximum_suppression.py
@@ -3,7 +3,7 @@ from __future__ import annotations  # type: ignore
 import itertools  # type: ignore
 import numpy as np  # type: ignore
 from nptyping import NDArray  # type: ignore
-from typing import Any, Tuple  # type: ignore
+from typing import Any, List  # type: ignore
 
 from metrics.base import Metric  # type: ignore
 from selection.base import Selector  # type: ignore
@@ -23,22 +23,16 @@ class DefaultNonMaximumSuppression:
     def transform(
         self,
         bounding_boxes: NDArray[(Any, 2, 2), np.float64]
-    ):
+    ) -> List[int]:
         bounding_box_ids = np.arange(0, bounding_boxes.shape[0])
-        bounding_box_ids_cp_iter = itertools.product(bounding_box_ids, bounding_box_ids)
-
-        ufs = []
-        bounding_box_ids_cp = []
-        for bids in bounding_box_ids_cp_iter:
-            ufs.append(self.metric.compute(bounding_boxes[bids[0]], bounding_boxes[bids[1]]) > self.metric_threshold)
-            bounding_box_ids_cp.append(bids)
+        bounding_box_ids_cp = itertools.product(bounding_box_ids, bounding_box_ids)
 
         unique_bids = set()
         unique_ufs_bids = set()
         bounding_box_ids_cp_ufs = []
-        for bids, flag in zip(bounding_box_ids_cp, ufs):
+        for bids in bounding_box_ids_cp:
             unique_bids.add(bids[0])
-            if flag:
+            if self.metric.compute(bounding_boxes[bids[0]], bounding_boxes[bids[1]]) > self.metric_threshold:
                 unique_ufs_bids.add(bids[0])
                 bounding_box_ids_cp_ufs.append(bids)
 
@@ -49,17 +43,16 @@ class DefaultNonMaximumSuppression:
         for bid in unique_ufs_bids:
             # Find overlapping boxes
             complementary_bids = [bids[1] for bids in bounding_box_ids_cp_ufs if bids[0] == bid]
-            cbid_ixs_to_remove = []
-            for ix, cbid in enumerate(complementary_bids):
-                if cbid in evaluated_bids or bid == cbid:
+            complementary_bids_filtered = []
+            for cbid in complementary_bids:
+                if cbid not in evaluated_bids and bid != cbid:
                     # If we've already seen this combination, then remove it from consideration
-                    cbid_ixs_to_remove.append(ix)
+                    complementary_bids_filtered.append(cbid)
                 evaluated_bids.add(cbid)
                 evaluated_bids.add(bid)
 
-            complementary_bids = [cbid for ix, cbid in enumerate(complementary_bids) if ix not in cbid_ixs_to_remove]
-            if len(complementary_bids) > 0:
-                selected_bid = self.selector.select(complementary_bids)
+            if len(complementary_bids_filtered) > 0:
+                selected_bid = self.selector.select(complementary_bids_filtered)
                 selected_bids.append(selected_bid)
 
         selected_bids = list(set(selected_bids))
