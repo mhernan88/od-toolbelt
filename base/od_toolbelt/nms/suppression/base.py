5c27d6bc53c2a9fd415e7a1f92d2669ea8ab0f61
Michael Hernandez <mhernan88@gmail.com>
2020-10-13 23:14:22 -0400

    Removed unused imports - also added back (and updated) batch and burst methods

--- base/od_toolbelt/nms/suppression/base.py
+++ base/od_toolbelt/nms/suppression/base.py
@@ -1,9 +1,7 @@
 # Copyright 2020 Michael Hernandez
 
-from typing import Any, Tuple, List  # type: ignore
+from typing import List  # type: ignore
 
-import numpy as np  # type: ignore
-from nptyping import NDArray  # type: ignore
 from od_toolbelt.nms.selection import Selector  # type: ignore
 from od_toolbelt.nms.metrics import Metric  # type: ignore
 from od_toolbelt import BoundingBoxArray  # type: ignore
@@ -39,57 +37,35 @@ class Suppressor:
         """
         raise NotImplementedError
 
-    # def burst(
-    #     self,
-    #     bounding_box_burst: List[NDArray[(Any, 2, 2), np.float64]],
-    #     confidences_burst: List[NDArray[(Any,), np.float64]],
-    #     labels_burst: List[NDArray[(Any,), np.int64]],
-    #     *args,
-    #     **kwargs
-    # ) -> Tuple[
-    #     NDArray[(Any, 2, 2), np.float64],
-    #     NDArray[(Any,), np.float64],
-    #     NDArray[(Any,), np.int64],
-    # ]:
-    #     """A method to run transform() on a burst of images.
-    #
-    #     All images in the burst should be take of the same subject from the same angle.
-    #
-    #     Args:
-    #         bounding_box_burst: A burst of images. Each element of this list represents an image's worth of
-    #             bounding boxes. All of the provided boxes will be included in the non-maximum suppression calculation
-    #             together.
-    #         confidences_burst: A burst of images. Each element of this list represents an image's worth of
-    #             confidences. Each confidence value corresponds to a specific bounding box in bounding_box_burst.
-    #
-    #     Returns:
-    #         A list of the selected bounding boxes.
-    #         A list of the selected confidences.
-    #     """
-    #     raise NotImplementedError
-    #
-    # def batch(
-    #     self,
-    #     bounding_box_batch: List[List[NDArray[(Any, 2, 2), np.float64]]],
-    #     confidences_batch: List[List[NDArray[(Any,), np.float64]]],
-    #     labels_batch: List[List[NDArray[(Any,), np.int64]]],
-    #     *args,
-    #     **kwargs
-    # ) -> Tuple[
-    #     List[NDArray[(Any, 2, 2), np.float64]],
-    #     List[NDArray[(Any,), np.float64]],
-    #     List[NDArray[(Any,), np.int64]],
-    # ]:
-    #     """A method to run burst() on a batch of different images.
-    #
-    #     Args:
-    #         bounding_box_batch: A batch of bursts of images. Each element of this list represents the bounding boxes
-    #         for a burst of images (see documentation for burst() method).
-    #         confidences_batch: A batch of bursts of images. Each element of this list represents the confidences
-    #         for a burst of images (see documentation for burst() method).
-    #
-    #     Returns:
-    #         A list of bounding box outputs from the burst() method.
-    #         A list of confidence outputs from the burst() method.
-    #     """
-    #     raise NotImplementedError
+    def burst(
+            self,
+            bounding_box_array_burst: BoundingBoxArray,
+    ) -> BoundingBoxArray:
+        """A method to run transform() on a burst of images.
+
+        All images in the burst should be take of the same subject from the same angle.
+
+        Args:
+            bounding_box_array_burst: A burst of images. Each element of this list represents an image's worth of
+                bounding boxes, confidences, and labels. All of the provided boxes will be included in the
+                non-maximum suppression calculation together.
+
+        Returns:
+            A single BoundingBoxArray object with the resulting bounding boxes, confidences, and labels.
+        """
+        raise NotImplementedError
+
+    def batch(
+            self,
+            bounding_box_array_batch: List[BoundingBoxArray]
+    ) -> BoundingBoxArray:
+        """A method to run burst() on a batch of different images.
+
+        Args:
+            bounding_box_array_batch: A batch of bursts of images. Each element of this list represents the
+             bounding boxes, confidences, and labels for a burst of images (see documentation for burst() method).
+
+        Returns:
+            A list of BoundingBoxArray objects from the burst() method.
+        """
+        raise NotImplementedError
