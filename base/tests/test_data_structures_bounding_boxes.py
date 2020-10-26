82b0fc1e85f27eb4a88b8775f8ca27f285f68696
Michael Hernandez <mhernan88@gmail.com>
2020-10-26 16:57:58 -0400

    Updated test cases for new setup function

--- base/tests/test_data_structures_bounding_boxes.py
+++ base/tests/test_data_structures_bounding_boxes.py
@@ -7,7 +7,7 @@ from .test_utils.test_setup import setup_test_case, one_additional
 
 def test_check1():
     """Test check without arguments"""
-    bounding_boxes, confidences, labels = setup_test_case()
+    bounding_boxes, confidences, labels, _ = setup_test_case()
 
     bb = BoundingBoxArray(
         bounding_boxes=bounding_boxes, confidences=confidences, labels=labels
@@ -18,7 +18,7 @@ def test_check1():
 
 def test_check2():
     """Test check with arguments"""
-    bounding_boxes, confidences, labels = setup_test_case()
+    bounding_boxes, confidences, labels, _ = setup_test_case()
     bounding_box, confidence, label = one_additional()
 
     bb = BoundingBoxArray(
@@ -30,7 +30,7 @@ def test_check2():
 
 def test_check3():
     """Test check without arguments - warn"""
-    bounding_boxes, confidences, labels = setup_test_case()
+    bounding_boxes, confidences, labels, _ = setup_test_case()
     bounding_boxes = bounding_boxes.astype(np.int64)
 
     bb = BoundingBoxArray(
@@ -43,7 +43,7 @@ def test_check3():
 
 def test_check4():
     """Test check without arguments - warn"""
-    bounding_boxes, confidences, labels = setup_test_case()
+    bounding_boxes, confidences, labels, _ = setup_test_case()
     confidences = confidences.astype(np.int64)
 
     bb = BoundingBoxArray(
@@ -56,7 +56,7 @@ def test_check4():
 
 def test_check5():
     """Test check without arguments - warn"""
-    bounding_boxes, confidences, labels = setup_test_case()
+    bounding_boxes, confidences, labels, _ = setup_test_case()
     labels = labels.astype(np.float64)
 
     bb = BoundingBoxArray(
@@ -69,7 +69,7 @@ def test_check5():
 
 def test_check6():
     """Test check with arguments - warn"""
-    bounding_boxes, confidences, labels = setup_test_case()
+    bounding_boxes, confidences, labels, _ = setup_test_case()
     bounding_box, confidence, label = one_additional()
     bounding_box = bounding_box.astype(np.int64)
 
@@ -83,7 +83,7 @@ def test_check6():
 
 def test_check7():
     """Test check without arguments - fail bb range"""
-    bounding_boxes, confidences, labels = setup_test_case()
+    bounding_boxes, confidences, labels, _ = setup_test_case()
     bounding_boxes[0, 0, 0] = 1.01
 
     bb = BoundingBoxArray(
@@ -96,7 +96,7 @@ def test_check7():
 
 def test_check9():
     """Test check without arguments - fail bb shape len"""
-    bounding_boxes, confidences, labels = setup_test_case()
+    bounding_boxes, confidences, labels, _ = setup_test_case()
     bounding_boxes = np.array(
         (
             ((0.04, 0.19, 0.12), (0.14, 0.29, 0.81)),
@@ -115,7 +115,7 @@ def test_check9():
 
 def test_append1():
     """Test append without arguments"""
-    bounding_boxes, confidences, labels = setup_test_case()
+    bounding_boxes, confidences, labels, _ = setup_test_case()
     before_length = bounding_boxes.shape[0]
 
     bounding_box, confidence, label = one_additional()
