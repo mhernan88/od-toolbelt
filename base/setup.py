185702a3396c066496b9b52dda977c639a220cd7
= <=>
2021-04-15 01:25:24 -0400

    Added cython build stuff

--- base/setup.py
+++ base/setup.py
@@ -1,8 +1,24 @@
-from setuptools import setup  # type: ignore
+import numpy as np # type: ignore
+from pathlib import Path # type: ignore
+from setuptools import Extension, setup  # type: ignore
+from Cython.Build import cythonize # type: ignore
+
+rootdir = Path(__file__).parent.absolute()
+
+extensions = [
+    Extension(
+        "od_toolbelt.nms.metrics.base",
+        [
+            Path(rootdir, "src", "base.pyx").as_posix()
+        ],
+        include_dirs=[np.get_include()]
+    ),
+]
 
 setup(
     name="odToolbelt",
     version="0.1.1",
+    include_package_data=True,
     packages=[
         "od_toolbelt",
         "od_toolbelt.data_structures",
@@ -17,4 +33,5 @@ setup(
     author="Michael Hernandez",
     author_email="",
     description="",
+    ext_modules=cythonize(extensions),
 )
